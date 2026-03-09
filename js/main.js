679e50dd05c0be15f4a5889017d17e6656e99861
Awais Farooq <114932015+captainawais@users.noreply.github.com>
2026-03-10 01:21:44 +0500

    Refactor loadSection function to fetch HTML content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+function loadSection(id,file){
 
-fetch("sections/hero.html")
+fetch(file)
 .then(res => res.text())
 .then(data => {
-document.getElementById("hero").innerHTML = data;
-});
+
+document.getElementById(id).innerHTML = data
+
+})
+
+}
+
+
+loadSection("navbar","sections/navbar.html")
+
+loadSection("hero","sections/hero.html")
+
+loadSection("about","sections/about.html")
+
+loadSection("skills","sections/skills.html")
+
+loadSection("projects","sections/projects.html")
+
+loadSection("achievements","sections/achievements.html")
+
+loadSection("certifications","sections/certifications.html")
+
+loadSection("education","sections/education.html")
+
+loadSection("blog","sections/blog.html")
+
+loadSection("contact","sections/contact.html")
+
+loadSection("footer","sections/footer.html")
